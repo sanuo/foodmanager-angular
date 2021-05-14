d1c3837eba819172207b568ab05110f8c4470460
呉 翔宇 <o_sanu@lassic.local>
2021-05-14 14:52:42 +0900

    view navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { TopPageComponent } from './top-page/top-page.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     SignupComponent,
-    TopPageComponent
+    TopPageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
