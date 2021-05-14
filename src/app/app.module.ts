00edbb966dcaeb773ecdd74ce0b5b72037b2a1ba
呉 翔宇 <o_sanu@lassic.local>
2021-05-14 14:31:53 +0900

    generate component login signup top-page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { TopPageComponent } from './top-page/top-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    TopPageComponent
   ],
   imports: [
     BrowserModule,
