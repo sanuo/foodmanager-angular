07bebe713cffbd62a1e25f2f867d37c98155a777
呉 翔宇 <o_sanu@lassic.local>
2021-05-14 14:37:17 +0900

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,16 +9,4 @@ import axios from 'axios';
 })
 export class AppComponent {
   title = 'foodmanager-angular';
-
-  //testAPI
-  onClickCallTestApi() {
-    axios.get("http://127.0.0.1:8000/api/mytest")
-    .then((res) => {
-      console.log(res)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-    // console.log('api test')
-  }
 }
