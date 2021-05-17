0e23a46919575bb36f747cecdf25a43aa52e0060
呉 翔宇 <o_sanu@lassic.local>
2021-05-17 11:38:14 +0900

    create AuthStateService

---
+++ src/app/shared/auth-state.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/internal/BehaviorSubject';
+import { TokenService } from '../shared/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthStateService {
+
+  constructor() { }
+}
