fdd285bc0789802981ff9549b9fc16c7edfe65b2
呉 翔宇 <o_sanu@lassic.local>
2021-05-18 14:47:53 +0900

    create category store

--- src/app/master/category/category.component.ts
+++ src/app/master/category/category.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Category } from 'src/app/model/category.model';
 import { MasterService } from 'src/app/shared/master/master.service';
 
@@ -8,17 +9,38 @@ import { MasterService } from 'src/app/shared/master/master.service';
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
-
+  
   categories: Category[];
+
+  categoryForm: FormGroup;
 
   constructor(
     private masterService: MasterService,
-  ) { }
+    public fb: FormBuilder,
+  ) {
+    this.categoryForm = this.fb.group({
+      name: [''],
+    })
+  }
 
   ngOnInit(): void {
     this.masterService.getCategories().subscribe((data:any) => {
       this.categories = data;
+
     })
+  }
+
+  onSubmit() {
+    this.masterService.storeCategory(this.categoryForm.value).subscribe(
+      result => {
+        // console.log(result)
+        this.categories.push(result);
+      },
+      error => {
+        // this.errors = error.error;
+        console.log(error.error);
+      }
+    )
   }
 
 }
--- src/app/shared/master/master.service.ts
+++ src/app/shared/master/master.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Category } from 'src/app/model/category.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,10 @@ export class MasterService {
   // get Categories
   getCategories(): Observable<any> {
     return this.http.get('http://127.0.0.1:8000/api/masters/category');
+  }
+
+  // store Category
+  storeCategory(category: Category): Observable<any> {
+    return this.http.post('http://127.0.0.1:8000/api/masters/category', category)
   }
 }
