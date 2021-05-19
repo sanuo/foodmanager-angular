2b6fb17aaab1c750519e3d1bd3c41e5a2ff80047
呉 翔宇 <o_sanu@lassic.local>
2021-05-19 11:03:23 +0900

    create food model

---
+++ src/app/model/food.model.ts
@@ -0,0 +1,9 @@
+// Food interface
+export interface Food {
+    id: number;
+    category_master_id: number;
+    user_id: number;
+    name: string;
+    created_at: string;
+    updated_at: string;
+  }
