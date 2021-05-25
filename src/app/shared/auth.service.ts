dd6af3e6da4427fa6e6ec9844edfbb184e696869
呉 翔宇 <o_sanu@lassic.local>
2021-05-25 10:11:00 +0900

    update environment

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../model/user.model';
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
@@ -13,17 +14,17 @@ export class AuthService {
 
   // User registration
   register(user: User): Observable<any> {
-    return this.http.post('https://foodmanager-backend.herokuapp.com/api/auth/register', user);
+    return this.http.post(environment.apiUrl + '/auth/register', user);
   }
 
   // Login
   signin(user: User): Observable<any> {
-    return this.http.post<any>('https://foodmanager-backend.herokuapp.com/api/auth/login', user);
+    return this.http.post<any>(environment.apiUrl + '/auth/login', user);
   }
 
   // Access user profile
   profileUser(): Observable<any> {
-    return this.http.get('https://foodmanager-backend.herokuapp.com/api/auth/user-profile');
+    return this.http.get(environment.apiUrl + '/auth/user-profile');
   }
   
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://foodmanager-backend.herokuapp.com/api'
 };
